b7c693ce27e2fb50d1d67258a5a73870efae6e89
sbkannadamoviebot <sbkannadamoviebot@gmail.com>
2026-01-26 15:00:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,15 +53,15 @@ FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://SBKANNADA:SBKANNAD
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "SBKANNADA")
 
 # Other Channel's
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1001864434358"))
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1003543883078"))
 DELETE_CHANNELS = int(environ.get("DELETE_CHANNELS", "0"))
-request_channel = environ.get("REQUEST_CHANNEL", "-1001864434358")
+request_channel = environ.get("REQUEST_CHANNEL", "-1003543883078")
 REQUEST_CHANNEL = (
     int(request_channel)
     if request_channel and id_pattern.search(request_channel)
     else None
 )
-MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", "-1001864434358"))
+MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", "-1003543883078"))
 
 # Added Link Here Not Id
 SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/SB_KANNADA_MOVE_WORLD")
