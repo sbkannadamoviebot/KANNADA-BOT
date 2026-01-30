705cab681d0a36de873ce8acbddbb2991337ade3
sbkannadamoviebot <sbkannadamoviebot@gmail.com>
2026-01-30 21:27:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ PORT = environ.get("PORT", "8082")
 # Owners
 ADMINS = [
     int(admin) if id_pattern.search(admin) else admin
-    for admin in environ.get("ADMINS", "5974804314").split()
+    for admin in environ.get("ADMINS", "5974804314,7087329103").split()
 ]
 OWNER_USERNAME = environ.get(
     "OWNER_USERNAME", "suryabm04"
